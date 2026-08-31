b937d6170659d6c72c1c70b69c1056fdd62463b3
ibenfarikh-lab <ibenfarikh@gmail.com>
2026-08-31 07:45:47 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -497,13 +497,13 @@ window.addEventListener('scroll', function() {
 }, true);
 
 function formatTeksDanAngka(input) {
-  let val = input.value;
+  let val = input.value.replace(/\./g, '');
   let formatted = val.replace(/(\d+)/g, (match) => {
-    let cleanNum = match.replace(/\./g, '');
-    return cleanNum.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+    return match.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
   });
   input.value = formatted;
 }
+
 
 function formatInputRupiah(input) {
   let angka = input.value.replace(/[^,\d]/g, '').toString();
